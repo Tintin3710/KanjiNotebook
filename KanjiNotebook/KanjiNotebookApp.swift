c2ecc5ec5f26c4db29754e0a0fa7c583bac4fa53
tintin <tintin@slicemind.kr>
2026-04-19 00:08:00 +0900

    Initial Commit

---
+++ KanjiNotebook/KanjiNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanjiNotebookApp.swift
+//  KanjiNotebook
+//
+//  Created by Tintin on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct KanjiNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
